1b53c09bf1bec3961f3e60e6cc00be1f6bef2643
johnzhao <johnzhao09@gmail.com>
2011-12-08 20:17:10 -0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test{
 	public static void main(String[] args){
 
 		Syetem.out.println("Hello World");
-
+System.out.println("!");
 		System.out.println("!");
 		System.out.println("my name is johnZ");
 

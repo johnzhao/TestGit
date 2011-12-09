7e7892740ff3ac839f75515f95aca5e235a52eeb
John <johnzhao09@gmail.com>
2011-12-08 18:46:09 -0800

    5th commit

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test{
 	public static void main(String[] args){
 		Syetem.out.println("Hello World");
 		System.out.println("!");
-		System.out.println("my name is john ");
+		System.out.println("my name is johnZ");
 	}
 }

709e83215528d8808f45e43b44faf4cf2ab132ff
John <johnzhao09@gmail.com>
2011-12-08 20:08:09 -0800

    hr

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+
+
+
+	public static void main(String[] args){
+
+		Syetem.out.println("Hello World");
+
+		System.out.println("!");
+		System.out.println("my name is johnZ");
+
+	}
+
+}

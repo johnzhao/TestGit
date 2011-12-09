94284958034ea22d71cc4d78f5f10b85a6f28ad7
John <johnzhao09@gmail.com>
2011-12-08 20:30:04 -0800

    Test2.java commit

---
+++ Test2.java
@@ -0,0 +1,14 @@
+public class Test2{
+
+
+
+	public static void main(String[] args){
+
+		Syetem.out.println("Hello World 2");
+		System.out.println("!  ");
+		System.out.println("!");
+		System.out.println("my name is jazz");
+
+	}
+
+}

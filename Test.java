7a5fd84e0a26d6096eb60ed61589b7671ff8f9f2
John Zhao <jozhao2@cisco.com>
2012-09-30 05:52:26 +0100

    delete empty spaces

--- Test.java
+++ Test.java
@@ -1,14 +1,9 @@
 public class Test{
 
-
-
 	public static void main(String[] args){
-
 		Syetem.out.println("Hello World");
 		System.out.println("!");
 		System.out.println("!");
 		System.out.println("my name is johnZ");
-
 	}
-
 }
